fc09e7dd7c72862cfeada7b682872fbefca03878
profmlr05 <mlazarodes@gmail.com>
2025-04-21 12:11:05 +0200

    Modificación desde la rama1 4

--- holamundogit/src/com/endes/pruebas/Holamundogit.java
+++ holamundogit/src/com/endes/pruebas/Holamundogit.java
@@ -2,8 +2,10 @@ package com.endes.pruebas;
 
 public class Holamundogit {
 	public static void main (String[] args) {
+		System.out.println("Hola mundo modificación 4 desde la rama1");
 		System.out.println("Hola mundo java-git");
 		System.out.println("Modificación 3 desde la rama1");
+		System.out.println("Hola mundo modificación 4 desde la rama1");
 		System.out.println("Hola mundo modificación 2 desde la rama1");
 		System.out.println("Modificaciones en la rama1");
 	}
