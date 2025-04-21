005902e70736e061d431e2866227dfd6a3f88a1e
profmlr05 <mlazarodes@gmail.com>
2025-04-21 10:47:17 +0200

    Modificación Holamundogit del conflicto de fusión.

--- holamundogit/src/com/endes/pruebas/Holamundogit.java
+++ holamundogit/src/com/endes/pruebas/Holamundogit.java
@@ -3,11 +3,8 @@ package com.endes.pruebas;
 public class Holamundogit {
 	public static void main (String[] args) {
 		System.out.println("Hola mundo java-git");
-<<<<<<< HEAD
 		System.out.println("Hola mundo desde la rama master");
-=======
 		System.out.println("Hola mundo modificación 2 desde la rama1");
->>>>>>> rama1
 		System.out.println("Modificaciones en la rama1");
 	}
 	
