11bf4fa9f2d53a2f85fc56e92e20813cd1272cf3
profmlr05 <mlazarodes@gmail.com>
2025-04-21 09:47:53 +0200

    Modificación de Holamundogit desde master

--- holamundogit/src/com/endes/pruebas/Holamundogit.java
+++ holamundogit/src/com/endes/pruebas/Holamundogit.java
@@ -3,6 +3,7 @@ package com.endes.pruebas;
 public class Holamundogit {
 	public static void main (String[] args) {
 		System.out.println("Hola mundo java-git");
+		System.out.println("Hola mundo desde la rama master");
 	}
 	
 }
