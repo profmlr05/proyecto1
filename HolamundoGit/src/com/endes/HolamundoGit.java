6b2a0381e3e991d46b5f7f1a1d5bf30a774406d0
profmlr05 <mlazarodes@gmail.com>
2025-05-12 20:03:10 +0200

    Modificación de clase.

--- HolamundoGit/src/com/endes/HolamundoGit.java
+++ HolamundoGit/src/com/endes/HolamundoGit.java
@@ -1,7 +1,10 @@
 package com.endes;
 
 public class HolamundoGit { 
+	
 	public static void main ( String[] arg) {
+		// Se añade línea para escribir un mensaje por consola.
+		System.out.println("Hola mundo con Eclipse,  Git y GitHub");
 		
 	}
 }
